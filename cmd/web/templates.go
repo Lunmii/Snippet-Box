1e09d80e02166d11ea8ab3a4006210bf8a25ce2f
Alo OLuwapelumi <lumii.dev@gmail.com>
2023-02-23 16:41:26 +0100

    embedding files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"Snippetbox/internal/models"
+	"Snippetbox/ui"
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 )
@@ -27,20 +29,20 @@ var functions = template.FuncMap{
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/partials/*.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/partials/*html",
+			page,
 		}
 
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
